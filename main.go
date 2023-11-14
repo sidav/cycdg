cc718febc4239c874ced436f66133055265631d1
Vladislav Kovun <sidav94@gmail.com>
2023-11-14 02:12:10 +0100

    Add more proper time measure for gen test.

--- main.go
+++ main.go
@@ -5,24 +5,48 @@ import (
 	"cycdg/lib/random"
 	"cycdg/lib/random/pcgrandom"
 	"cycdg/lib/tcell_console_wrapper"
+	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 var (
-	cw  tcell_console_wrapper.ConsoleWrapper
-	rnd random.PRNG
+	cw               tcell_console_wrapper.ConsoleWrapper
+	rnd              random.PRNG
+	testResultString string
 )
 
 func main() {
-	cw.Init()
-	defer cw.Close()
+	defer func() { fmt.Printf(testResultString) }()
+
 	rnd = pcgrandom.NewPCG64()
 	rnd.SetSeed(int(time.Now().UnixNano()))
-
 	graph.SetRandom(rnd)
+
+	if len(os.Args) > 1 {
+		if len(os.Args) < 4 {
+			fmt.Println("TEST USAGE: go run *.go [graph size] [total generations] [desired fill percentage]")
+			return
+		}
+		size, err1 := strconv.Atoi(os.Args[1])
+		tests, err2 := strconv.Atoi(os.Args[2])
+		fill, err3 := strconv.Atoi(os.Args[3])
+		if err1 != nil || err2 != nil || err3 != nil {
+			fmt.Println("Please use numbers as args.")
+			return
+		}
+		testGen(size, tests, fill)
+		return
+	} else {
+		testGen(5, 1000, 100)
+	}
+
+	cw.Init()
+	defer cw.Close()
+
 	gr := &graph.Graph{}
 
-	testGen(gr)
 	key := ""
 	for key != "ESCAPE" {
 		if key == "" || gr.GetFilledNodesPercentage() > 65 {
@@ -36,11 +60,24 @@ func main() {
 	}
 }
 
-func testGen(gr *graph.Graph) {
-	for i := 0; i < 1000; i++ {
-		gr.InitWithConnectedNodes(5, 5)
-		for gr.GetFilledNodesPercentage() < 100 {
+func testGen(size, tests, fillPerc int) {
+	if fillPerc > 100 {
+		panic("Inadequate fill percentage")
+	}
+	var appliedRules int
+	gr := &graph.Graph{}
+
+	start := time.Now()
+	for i := 0; i < tests; i++ {
+		gr.InitWithConnectedNodes(size, size)
+		for gr.GetFilledNodesPercentage() < fillPerc {
 			gr.AlterSomething()
 		}
+		appliedRules += gr.AppliedRulesCount
 	}
+	totalGenTime := time.Since(start)
+	testResultString = fmt.Sprintf("TEST: Total %d graphs of size %dx%d, filled for %d percents\n", tests, size, size, fillPerc)
+	testResultString += fmt.Sprintf("Total time %v, mean time per single gen %v\n", totalGenTime, totalGenTime/time.Duration(tests))
+	testResultString += fmt.Sprintf("Total rules applied %d, mean time per rule %v\n",
+		appliedRules, totalGenTime/time.Duration(appliedRules))
 }
