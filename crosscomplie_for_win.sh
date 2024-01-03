260cb49155053ea93fa429c375f03df7d7d88042
Vladislav Kovun <sidav94@gmail.com>
2024-01-03 23:11:11 +0100

    Improve cross-compile script.

--- crosscomplie_for_win.sh
+++ crosscomplie_for_win.sh
@@ -1,1 +1,1 @@
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o cycdg$(date '+%d-%m-%Y_%H').exe *.go
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags="-s -w" -o cycdg$1$(date '+%d-%m-%Y_%H').exe *.go
